ae7893dad341ce8569c8f6d48b3609b31cfffa20
damiancra <damiancra@gmail.com>
2022-04-23 15:38:40 +0100

    MusicDatabase

--- src/main/java/com/example/music/MusicApplication.java
+++ src/main/java/com/example/music/MusicApplication.java
@@ -2,6 +2,7 @@ package com.example.music;
 
 import org.springframework.boot.SpringApplication;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/test/java/com/example/music/service/MusicServiceDBTest.java
+++ src/test/java/com/example/music/service/MusicServiceDBTest.java
@@ -3,6 +3,7 @@ package com.example.music.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
